093961c60b2e142a9944b3bf86fe90220badd45c
Ajmine Adil <ajmineadil@gmail.com>
2026-09-23 20:55:56 +0600

    Lab 01 - Largest Numb

---
+++ LargestNumb.java
@@ -0,0 +1,15 @@
+public class LargestNumb {
+        public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        if (a > b && a > c) {
+            System.out.println("a is the largest");
+        } else if (c > b && c > a) {
+            System.out.println("c is the largest");
+        } else {
+            System.out.println("b is the largest");
+        }
+    }
+}
