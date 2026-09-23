0e74a154dbaa02bd849dd5be7d10b3ef63a54f42
Ajmine Adil <ajmineadil@gmail.com>
2026-09-23 20:55:02 +0600

    Lab 01 - Basic print

---
+++ Lab01_Basic_Print.java
@@ -0,0 +1,12 @@
+public class Lab01_Basic_Print {
+    
+    public static void main(String[] args) {
+        System.out.println("hello ajmine");
+        int a = 5;
+        double d = 6.77;
+        String s = "Hi everyone";
+        
+        System.out.println(s + '\n' + a +' '+ d);
+    }
+}
+
---
+++ String01.java
@@ -0,0 +1,9 @@
+public class String01 {
+    
+    public static void main(String[] args) {
+        String Name = "Ajmine Adil Sadik";
+        int length = Name.length();
+        System.out.println(length);
+    
+}
+}
