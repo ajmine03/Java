0566bcf6de37e5f4e55f059180365f6d4acce0f8
AJMINE_172 <ajmineadil@gmail.com>
2026-09-26 15:53:19 +0600

    Add files via upload

---
+++ Lab03_1.java
@@ -0,0 +1,13 @@
+package com.mycompany.lab03;
+
+public class Lab03_1 {
+
+    String name;
+    String[] courses = {"oop", "ec"};
+    Student s1 = new Student();
+
+    void showStudentInfo() {
+        s1.name = "rezuan";
+        s1.displayInfo();
+    }
+}
