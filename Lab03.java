968f81db70a7c116bd662f27f4b23d9f76e4d346
AJMINE_172 <ajmineadil@gmail.com>
2026-09-26 15:52:49 +0600

    Add files via upload

---
+++ Lab03.java
@@ -0,0 +1,47 @@
+package com.mycompany.lab03;
+
+public class Lab03 {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        
+        s1.name = "Ajmine";
+        s1.id = "252-15-172";
+        s1.section = "I-2 Lab";
+        s1.address = "DSC";
+
+        s1.read();
+        s1.displayInfo();
+        
+        Student s2 = new Student();
+  
+        s2.name = "Naim";
+        s2.id = "252-15-178";
+        s2.section = "I-2 Lab";
+        s2.address = "DSC";
+        
+        
+        s2.read();
+        s2.displayInfo();
+        
+    }
+}
+
+class Student {
+
+    String name;
+    String id;
+    String section;
+    String address;
+
+    void read() {
+        System.out.println("information:::");
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Section: " + section);
+        System.out.println("Address: " + address);
+    }
+}
